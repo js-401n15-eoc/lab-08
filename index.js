d3687fbbd44524b0e56ba22733ec88d03dc01c69
Earl Jay Caoile <unfie555@gmail.com>
2020-02-02 13:31:24 -0800

    stopping point: wrote a test for req props

--- index.js
+++ index.js
@@ -2,5 +2,10 @@
 
 require('dotenv').config();
 const server = require('./lib/server.js');
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
 server.start(process.env.PORT || 3000);
